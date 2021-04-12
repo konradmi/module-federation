e39d470ad90420d23241232cba7d7851d995a8ce
KonradM <miekus.konrad@gmail.com>
2021-04-12 08:20:56 +0200

    add a comment

--- packages/container/config/webpack.prod.js
+++ packages/container/config/webpack.prod.js
@@ -10,6 +10,8 @@ const devConfig = {
   mode: 'production',
   output: {
     filename: '[name].[contenthash].js',
+    // publicPath/filename is what webpack will use when injecting the script tag in /public/index.html
+    // The reason for that is that in cloudfront index.html and filename wont be in the same dir
     publicPath: '/container/latest/'
   },
   plugins: [
